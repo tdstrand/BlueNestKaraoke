081cfbeaa0952824a6ed531de48d6a6054502ef4
tstrand@strandcentral.com <tstrand@strandcentral.com>
2025-03-13 09:12:57 -0500

    8

---
+++ PasswordHash.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var hasher = new PasswordHasher<string>();
+        var hashedPassword = hasher.HashPassword("user", "pwd");
+        Console.WriteLine(hashedPassword);
+    }
+}
